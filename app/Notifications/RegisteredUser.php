7d188d91f02d427bee30e17cd673ff315c61f244
Sawers <acer.iconia.dak@gmail.com>
2016-10-11 13:19:48 +0200

    Add files via upload

---
+++ app/Notifications/RegisteredUser.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class RegisteredUser extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Registration of my website.')
+            ->line('Your account has been created. To login your account you have confirm by the URL we sended you by email.')
+            ->action('Activate my account', url("/confirm/{$notifiable->id}/". urlencode ($notifiable->confirmation_token)))
+            ->line('Thank you for using our application! If you are not the owner of this account, please delete this email.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
